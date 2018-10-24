8d44096739181fc4691b09d32343deb2f9ab6fef
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-10-24 16:10:40 -0700

    connected a map component that will cover page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import GoogleMap from './Map.js'
+import Map from './Map.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-
-        </div>
-        <script async defer src="https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyDXbgkEoSEdXPyG7GDmuf7Mb1wW8RZ1Ek4&v=3&callback=initMap">
-        </script>
+        <Map />
       </div>
     );
   }
