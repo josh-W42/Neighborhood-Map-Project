cd33bcc408efc4f33fb7ae7bc4f61fc064c2647f
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-10-24 15:28:30 -0700

    intializing git repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import GoogleMap from './Map.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+
+        </div>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyDXbgkEoSEdXPyG7GDmuf7Mb1wW8RZ1Ek4&v=3&callback=initMap">
+        </script>
       </div>
     );
   }
