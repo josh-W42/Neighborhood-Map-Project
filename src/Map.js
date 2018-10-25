21c4d8900d6edbf527856ac81e08af9aaf16c010
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-10-25 15:33:07 -0700

    map is now working with current possition set at UCLA

--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends Component {
 
 
-class Map extends Component {
+  onMarkerClick () {
+  }
+
   render() {
     return (
-      <div id="Map"></div>
-    )
+      <Map
+        google={this.props.google}
+        zoom={14}
+        initialCenter={{
+            lat: 34.069064,
+            lng: -118.445050
+          }}
+        >
+
+        <Marker onClick={console.log(this.onMarkerClick)}
+                animation={this.props.google.maps.Animation.DROP}
+                name={'Current location'} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>Hello</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
   }
 }
 
-
-export default Map
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDXbgkEoSEdXPyG7GDmuf7Mb1wW8RZ1Ek4'
+  // libraries: ['places', 'geometry']
+})(MapContainer)
