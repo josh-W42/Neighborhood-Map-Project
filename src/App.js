2b19ebb13376e3993dcefb54af6c7ae1aa52235e
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-10-29 22:25:20 -0700

    added in the code for a new compenent and passed google api object as a property

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './Map.js';
+import {GoogleApiWrapper} from 'google-maps-react';
+import SideUI from './SideUI.js';
 
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div className="App">
-        <Map />
+        <SideUI google={this.props.google} />
+        <Map google={this.props.google} />
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDXbgkEoSEdXPyG7GDmuf7Mb1wW8RZ1Ek4'
+  // libraries: ['places', 'geometry']
+})(App)
