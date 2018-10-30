d6ef3055894beed19369a95fed59225063cdfbfc
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-10-29 22:24:11 -0700

    added in the sideUI component

---
+++ src/SideUI.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SideUI extends Component {
+
+  render() {
+    return (
+      <div id="sideUI">
+        <h1>Hungry?</h1>
+        <input
+          type="text"
+          />
+      </div>
+    );
+  }
+}
+
+export default SideUI
