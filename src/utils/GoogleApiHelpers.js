35a3e9dc0eb815ce52eeedf7da879c0c59c5d36b
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-05 16:34:32 -0800

    added this utlity script that will store google map api functions

---
+++ src/utils/GoogleApiHelpers.js
@@ -0,0 +1,29 @@
+export function searchNearby(google, map, request) {
+  return new Promise((resolve, reject) => {
+    const service = new google.maps.places.PlacesService(map);
+
+    service.nearbySearch(request, (results, status, pagination) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+        resolve(results, pagination);
+      } else {
+        reject(results, status);
+      }
+    })
+  });
+}
+
+export function searchFor(google, map, request) {
+  return new Promise((resolve, reject) => {
+    const service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+        resolve(results);
+      } else {
+        reject(results, status);
+      }
+    })
+  });
+}
