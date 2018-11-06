1e3d0cf9eb665c64579e72d385fc915ea6fa4fd5
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-05 16:39:44 -0800

    modified google map component and dependancies

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Map from './Map.js';
 import {GoogleApiWrapper} from 'google-maps-react';
-import SideUI from './SideUI.js';
 
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <SideUI google={this.props.google} />
         <Map google={this.props.google} />
       </div>
     );
