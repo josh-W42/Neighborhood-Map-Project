e62cb9ad0890aac30d97ffcfd968e899cb68d006
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-10 02:14:24 -0800

    now updates based off of geolocation and will set as map center, default is san fran

--- src/SideUI.js
+++ src/SideUI.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import { searchFor } from './utils/GoogleApiHelpers.js'
+import { searchFor, searchNearby } from './utils/GoogleApiHelpers.js'
 
 
 class SideUI extends Component {
 
   onSearch(input) {
-    const map = this.props.map;
 
+    const map = this.props.map;
     const {google} = this.props;
     const options = {
+      input: input,
       location: map.center,
-
+      radius: '500'
     }
     searchFor(google, map, options)
+    .then((results) => {
+      this.props.onMapUpdate(results);
+    }).catch((response, status) => {
+      console.log(status);
+      console.log(response);
+    });
+  }
+
+  onNearbySearch(catagory) {
+
+    const map = this.props.map;
+    const {google} = this.props;
+
+    let options = {
+      location: map.center,
+      radius: '2000',
+      rankby: 'distance'
+    }
+
+    switch (catagory) {
+      case 'food':
+        options.keyword = ['food'];
+        break;
+      case 'beauty':
+        options.keyword = ['beauty', 'hair_salon'];
+        break;
+      case 'banking':
+        options.keyword = ['finance', 'bank', 'atm'];
+        break;
+      case 'educational':
+        options.keyword = ['school', 'library', 'museum'];
+        break;
+      case 'entertainment':
+        options.keyword = ['stadium', 'campground'];
+        break;
+      default:
+        alert("i'm not working");
+    }
+
+    searchNearby(google, map, options)
     .then((results) => {
       this.props.onMapUpdate(results);
     }).catch((response, status) => {
@@ -30,6 +71,8 @@ class SideUI extends Component {
           <form>
             <input className="searchInput" type="text" placeholder="Name of Venue or Address"/>
             <input
+              className="searchSubmitBtn"
+              value="Search"
               type="submit"
               onClick={(e) => {
                 e.preventDefault();
@@ -37,30 +80,62 @@ class SideUI extends Component {
               }} />
           </form>
 
-          <h2>Get results by type:</h2>
-          <form>
-            <input type="radio" name="searchType" id="Food" value="Food" defaultChecked />
-            <label for="Food">Food</label>
-
-            <input type="radio" name="searchType" id="Beauty" value="Beauty" />
-            <label for="Beauty">Beauty</label>
-
-            <input type="radio" name="searchType" id="Finances" value="Finances" />
-            <label for="Finances">Finances</label>
-
-            <input type="radio" name="searchType" id="Entertainment" value="Entertainment" />
-            <label for="Entertainment">Entertainment</label>
-
-            <input type="radio" name="searchType" id="Educational" value="Educational" />
-            <label for="Educational">Educational</label>
+          <h2>Search Nearby results by type:</h2>
+          <form onChange={ (e) => this.onNearbySearch(e.target.value) }>
+            <table id="inputTable">
+              <tr>
+                <td>
+                  <input type="radio" name="searchType" id="food" value="food" defaultChecked />
+                  <label for="food">Food</label>
+                </td>
+                <td>
+                  <input type="radio" name="searchType" id="banking" value="banking" />
+                  <label for="banking">Banking</label>
+                </td>
+                <td>
+                  <input type="radio" name="searchType" id="beauty" value="beauty" />
+                  <label for="beauty">Beauty</label>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="radio" name="searchType" id="entertainment" value="entertainment" />
+                  <label for="entertainment">Entertainment</label>
+                </td>
+                <td>
+                  <input type="radio" name="searchType" id="educational" value="educational" />
+                  <label for="educational">Educational</label>
+                </td>
+              </tr>
+            </table>
           </form>
 
           <h2>Results:</h2>
           <div className="resultsContainer">
             {this.props.places.map( (place) => (
-              <div key={place.id} className="result">
-                <img className="businessImage" src={place.icon} alt={place.name} />
-                <h3 className="businessName">{place.name}</h3>
+              <div
+                key={place.id}
+                className="result"
+                onClick={() => {
+                  this.props.map.panTo({
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng()
+                  });
+                }}
+                >
+                <img
+                  className="businessImage"
+                  src={place.icon}
+                  alt={place.name} />
+                <div className='businessInfo'>
+                  <h3 className="businessName">{place.name}</h3>
+                  <p className="businessAddress">
+                    { (place.vicinity) ? place.vicinity:place.formatted_address }
+                  </p>
+                  <p className='businessOpen'>
+                    { (place.opening_hours) ? `Is currently: ${(place.opening_hours.open_now) ? 'Open':'Closed' }` : ''}
+                  </p>
+                </div>
               </div>
             ))}
           </div>
