d48d7707b0215a63f2c6f490f97d0d256c06e6e0
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-13 15:00:05 -0800

    added event handlers for tab indexing and added some aria labels

--- src/App.js
+++ src/App.js
@@ -4,8 +4,31 @@ import Map from './Map.js';
 import {GoogleApiWrapper} from 'google-maps-react';
 import { register } from './serviceWorker.js';
 
-register('./');
+//Code to use envrirnment variables
+require('dotenv').config()
+// Registering service worker
+register();
 
+// These two functions to handle user tabbing cases.
+// If the user is using tabs to use the app then focus is present.
+// If the user is not using tabs, then
+function handleFirstTab(e) {
+  if (e.keyCode === 9) {
+    document.body.classList.add('user-is-tabbing');
+
+    window.removeEventListener('keydown', handleFirstTab);
+    window.addEventListener('mousedown', handleMouseDownOnce);
+  }
+}
+
+function handleMouseDownOnce() {
+  document.body.classList.remove('user-is-tabbing');
+
+  window.removeEventListener('mousedown', handleMouseDownOnce);
+  window.addEventListener('keydown', handleFirstTab);
+}
+
+window.addEventListener('keydown', handleFirstTab);
 
 export class App extends Component {
 
@@ -27,14 +50,15 @@ export class App extends Component {
     return (
       <div className="App">
         <div className="topSection">
-          <div className="hamburger" onClick={this.onHamburgerClick}>
+          <div role="button" aria-label="Menu Toggle" tabIndex="0" className="hamburger cross" onClick={this.onHamburgerClick}>
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
           </div>
-          <h1>Neighborhood Map</h1>
+          <h1  tabIndex="0">Neighborhood Map</h1>
         </div>
-        <Map google={this.props.google} />
+        <Map
+          google={this.props.google} />
       </div>
     );
   }
