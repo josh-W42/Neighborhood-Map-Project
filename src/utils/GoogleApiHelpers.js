d9cfe26a9120e026f87acf2195d09a3091b65c5f
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-11-17 18:35:13 -0800

    this js file will help keep code clean in the other js files, just stores backup locations

--- src/utils/GoogleApiHelpers.js
+++ src/utils/GoogleApiHelpers.js
@@ -14,6 +14,25 @@ export function searchNearby(google, map, request) {
   });
 }
 
+
+export function reverseGeoCode(google, location) {
+  return new Promise((resolve, reject) => {
+    const service = new google.maps.Geocoder();
+
+    service.geocode({location: location}, (results, status) => {
+      if(status === google.maps.places.PlacesServiceStatus.OK) {
+        if (results[0]) {
+          resolve(results);
+        } else {
+          console.log("No results for location");
+        }
+      } else {
+        reject(status);
+      }
+    })
+  })
+}
+
 export function searchFor(google, map, request) {
   return new Promise((resolve, reject) => {
     const service = new google.maps.places.PlacesService(map);
---
+++ src/utils/backupLocations.js
@@ -0,0 +1,410 @@
+const backupResults = {
+  trending: [
+    {
+      venue: {
+        id: "4bd9c88767b49c74d89d2214",
+        name: "Pica Pica Arepa Kitchen",
+        categories: [
+          {
+            name: "Arepa Restaurant",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/food/arepas_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "401 Valencia St (at 15th St)",
+            "San Francisco, CA 94103",
+            "United States"
+          ],
+          lat: 37.766527521845696,
+          lng: -122.42194505584449
+        },
+      }
+    },
+    {
+      venue: {
+        id: "56299784498e7d0748ad8f9c",
+        name: "Oak + Fort",
+        categories: [
+          {
+            name: "Boutique",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/shops/apparel_boutique_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "567 Hayes St",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.776512857458314,
+          lng: -122.42575355943403
+        },
+      }
+    },
+    {
+      venue: {
+        id: "4bfdd7772b83b71364cba998",
+        name: "City Dance Annex",
+        categories: [
+          {
+            name: "Dance Studio",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/arts_entertainment/performingarts_dancestudio_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "1420 Harrison St (btwn 10th & 11th Sts)",
+            "San Francisco, CA 94103",
+            "United States"
+          ],
+          lat: 37.77141709937545,
+          lng: -122.41178415825729
+        },
+      }
+    },
+    {
+      venue: {
+        id: "4f04b8028b81d71b4cc4685f",
+        name: "CatHead's BBQ",
+        categories: [
+          {
+            name: "BBQ Joint",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/food/bbqalt_",
+              suffix: ".png"
+            }
+          }
+        ],
+        delivery: {
+          url: "https://www.grubhub.com/restaurant/catheads-bbq-1665-folsom-st-san-francisco/545765?affiliate=1131&utm_source=foursquare-affiliate-network&utm_medium=affiliate&utm_campaign=1131&utm_content=545765",
+          provider: {
+            icon: {
+              prefix: "https://fastly.4sqi.net/img/general/cap/",
+              sizes: [40, 50],
+              name: "/delivery_provider_grubhub_20180129.png"
+            },
+            name: "grubhub"
+          }
+        },
+        location: {
+          formattedAddress: [
+            "1665 Folsom St (at 12th St)",
+            "San Francisco, CA 94103",
+            "United States"
+          ],
+          lat: 37.77004677429094,
+          lng: -122.41533581150838
+        },
+      }
+    },
+    {
+      venue: {
+        id: "4a44417af964a52062a71fe3",
+        name: "Basil Canteen",
+        categories: [
+          {
+            name: "Thai Restaurant",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/food/thai_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "1489 Folsom St (at 11th)",
+            "San Francisco, CA 94103",
+            "United States"
+          ],
+          lat: 37.77186387844387,
+          lng: -122.41373419761656
+        },
+      }
+    },
+  ],
+
+  food: [
+    {
+      venue: {
+        id: "4a44417af964a52062a71fe3",
+        name: "Basil Canteen",
+        categories: [
+          {
+            name: "Thai Restaurant",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/food/thai_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "1489 Folsom St (at 11th)",
+            "San Francisco, CA 94103",
+            "United States"
+          ],
+          lat: 37.77186387844387,
+          lng: -122.41373419761656
+        },
+      }
+    },
+    {
+      venue: {
+        id: "435d7580f964a5201a291fe3",
+        name: "Zuni Café",
+        categories: [
+          {
+            name: "New American Restaurant",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/food/newamerican_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "1658 Market St (at Rose St)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.77364852834092,
+          lng: -122.42156041354832
+        },
+      }
+    },
+    {
+      venue: {
+        id: "500088f7d63e64b62bc19e6e",
+        name: "Rich Table",
+        categories: [
+          {
+            name: "New American Restaurant",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/food/newamerican_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "199 Gough St (at Oak St)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.774891,
+          lng: -122.422736
+        },
+      }
+    },
+    {
+      venue: {
+        id: "5907aa1c178a2a76066e0f59",
+        name: "a Mano",
+        categories: [
+          {
+            name: "Italian Restaurant",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/food/italian_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "450 Hayes St (Octavia Street)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.77674005696396,
+          lng: -122.42392434798019
+        },
+      }
+    },
+  ],
+
+  arts: [
+    {
+      venue: {
+        id: "4aa48566f964a520024720e3",
+        name: "Louise M. Davies Symphony Hall",
+        categories: [
+          {
+            name: "Concert Hall",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/arts_entertainment/musicvenue_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "201 Van Ness Ave (btwn Grove & Hayes St)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.7779963603856,
+          lng: -122.42039900756977
+        },
+      }
+    },
+    {
+      venue: {
+        id: "4b2e994af964a5205ee324e3",
+        name: "San Francisco Ballet",
+        categories: [
+          {
+            name: "Dance Studio",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/arts_entertainment/performingarts_dancestudio_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "455 Franklin St (btw Fulton & Grove)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.77858023622605,
+          lng: -122.42079814446592
+        },
+      }
+    },
+    {
+      venue: {
+        id: "43601880f964a5202e291fe3",
+        name: "Asian Art Museum",
+        categories: [
+          {
+            name: "Art Museum",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/arts_entertainment/museum_art_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "200 Larkin St (at McAllister St)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.780177691822274,
+          lng: -122.41650543857212
+        },
+      }
+    },
+    {
+      venue: {
+        id: "548b37a3498ee6785a1468df",
+        name: "Soundbox",
+        categories: [
+          {
+            name: "Music Venue",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/arts_entertainment/musicvenue_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "300 Franklin St. (Hayes St.)",
+            "San Francisco, CA 94103",
+            "United States"
+          ],
+          lat: 37.77734400229576,
+          lng: -122.4212225310717
+        },
+      }
+    },
+  ],
+
+  shops: [
+    {
+      venue: {
+        id: "4b7169c0f964a52069452de3",
+        name: "Acrimony",
+        categories: [
+          {
+            name: "Clothing Store",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/shops/apparel_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "333 Hayes St #100 (btwn Franklin & Gough St)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.77702506859015,
+          lng: -122.42199194990916
+        },
+      }
+    },
+    {
+      venue: {
+        id: "4b33e68af964a520822125e3",
+        name: "Fiddlesticks",
+        categories: [
+          {
+            name: "Kids Store",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/shops/apparel_kids_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "540 Hayes St",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.77664376825955,
+          lng: -122.42534076586571
+        },
+      }
+    },
+    {
+      venue: {
+        id: "4fb6d17fe4b0f61106031be8",
+        name: "Marine Layer",
+        categories: [
+          {
+            name: "Clothing Store",
+            icon: {
+              prefix: "https://ss3.4sqi.net/img/categories_v2/shops/apparel_",
+              suffix: ".png"
+            }
+          }
+        ],
+        location: {
+          formattedAddress: [
+            "498 Hayes St (Octavia)",
+            "San Francisco, CA 94102",
+            "United States"
+          ],
+          lat: 37.776689298514675,
+          lng: -122.42454666438235
+        },
+      }
+    },
+  ]
+}
+
+export default backupResults;
